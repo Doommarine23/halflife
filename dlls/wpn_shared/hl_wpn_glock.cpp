779f7b84b39baa86f7fa68b3cd565abecf15b3c5
Unknown <Doom.Marine23@gmail.com>
2018-06-03 12:51:28 -0400

    Glock reload times increased
    The glock reloaded way too fast in the logic compared to the animation. This is subject to change as I produce new animations. But right now this times up better with Valve's animations.

--- dlls/wpn_shared/hl_wpn_glock.cpp
+++ dlls/wpn_shared/hl_wpn_glock.cpp
@@ -183,9 +183,9 @@ void CGlock::Reload( void )
 	int iResult;
 
 	if (m_iClip == 0)
-		iResult = DefaultReload( 17, GLOCK_RELOAD, 1.5 );
+		iResult = DefaultReload( 17, GLOCK_RELOAD, 2.0 );
 	else
-		iResult = DefaultReload( 17, GLOCK_RELOAD_NOT_EMPTY, 1.5 );
+		iResult = DefaultReload( 17, GLOCK_RELOAD_NOT_EMPTY, 2.0 );
 
 	if (iResult)
 	{
